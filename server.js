e25265adf1f0307eadac8f5990ece3e7c0fadb9a
Daniel Ezekiel <dbezekiel77@gmail.com>
2022-08-24 16:45:33 +0100

    first api commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express =  require('express');
+const app = express();
+const PORT = 8000;
+
+const rappers = {
+    'eminem': {
+        birthName: 'Marshall Bruce Mathers III',
+        age: 49,
+        birthPlace: 'Saint Joseph, Missouri'
+    },
+    '50 cent': {
+        birthName: 'Curtis James Jackson III',
+        age: 47,
+        birthPlace: 'Queens, New York'
+    },
+    'jay z': {
+        birthName: 'Shawn Corey Carter',
+        age: 49,
+        birthPlace: 'Brooklyn, New York'
+    },
+    'post malone': {
+        birthName: 'Austin Richard Post',
+        age: 27,
+        birthPlace: 'Syracuse, New York'
+    },
+    'no name': {
+        birthName: 'Wrong/No Input',
+        age: 'Wrong/No Input',
+        birthPlace: 'Wrong/No Input'
+    }
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rappaName', (request, response) => {
+    const rapperName = request.params.rappaName.toLowerCase();
+    rappers[rapperName] ? response.json(rappers[rapperName]) : response.json(rappers['no name']);
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT + '. Go catch it!')
+})
